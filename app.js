4528b4474b67a7ec4a0f11d0f897a0edbbfde8f9
Luiz Santos <luiz.phelipe.800@gmail.com>
2021-09-01 03:59:25 -0300

    configuraçao do cors e dos logs com morgan

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const routes = require('./src/routes')
 const passport = require('./src/middlewares/passport')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
+app.use(cors())
+app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(routes)
