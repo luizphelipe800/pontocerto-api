b30cdc6c72db199d7fbf7711add54517d46ae773
Luiz Santos <luiz.phelipe.800@gmail.com>
2021-09-03 23:23:25 -0300

    configurando o mongodb para produçao

--- src/configs/mongoose.js
+++ src/configs/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://pontocerto-db:27017/pontocerto', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL_PRODUCTION, { useNewUrlParser: true, useUnifiedTopology: true })
 
 mongoose.connection.once('open', () => console.log('mongodb está de boa'))
 mongoose.connection.on('error', () => console.log('deu xabu no mongodb'))
