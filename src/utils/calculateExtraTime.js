4610556ac975f91f14cb4f13b5b50c8e14219ac3
Luiz Santos <luiz.phelipe.800@gmail.com>
2021-09-10 13:06:05 -0300

    corrigido falhas nos calculos de horas extras

--- src/utils/calculateExtraTime.js
+++ src/utils/calculateExtraTime.js
@@ -9,28 +9,36 @@ const { DateTime, Duration } = require('luxon')
  */
 
 const calculateExtraTime = (times, userTime) => {
-    const normalDuration = calculateDuration([userTime[0], '12:00', '13:00', userTime[1]])
-    const duration = calculateDuration(times)
-
-    const diff = duration.minus(normalDuration)
-
-    return diff.toFormat('hh:mm')
+    try {
+        const normalDuration = calculateDuration([userTime[0], '12:00', '13:00', userTime[1]])
+        const duration = calculateDuration(times)
+    
+        const diff = duration.minus(normalDuration)
+    
+        return diff.toFormat('hh:mm')
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
 const calculateDuration = horarios => {
-    const formato = 'hh:mm'
-
-    let inicio = DateTime.fromFormat(horarios[0], formato)
-    let fim = DateTime.fromFormat(horarios[3], formato)
-
-    let worked = Duration.fromMillis(fim.diff(inicio).milliseconds)
-
-    inicio = DateTime.fromFormat(horarios[1], formato)
-    fim = DateTime.fromFormat(horarios[2], formato)
-
-    let dinner = Duration.fromMillis(fim.diff(inicio).milliseconds)
-
-    return worked.minus(dinner)
+    try{
+        const formato = 'hh:mm'
+    
+        let inicio = DateTime.fromFormat(horarios[0], formato)
+        let fim = DateTime.fromFormat(horarios[3], formato)
+    
+        let worked = Duration.fromMillis(fim.diff(inicio).milliseconds)
+    
+        inicio = DateTime.fromFormat(horarios[1], formato)
+        fim = DateTime.fromFormat(horarios[2], formato)
+    
+        let dinner = Duration.fromMillis(fim.diff(inicio).milliseconds)
+    
+        return worked.minus(dinner)
+    }catch(error){
+        console.log(error.message)
+    }
 }
 
 module.exports = calculateExtraTime
